fc30ba9620e9dbe6ecf12d6e2d2f77a3ac35d3d9
Mikko Kallio <mikko.m.kallio@gmail.com>
2021-01-03 00:54:20 +0200

    Make app bar horizontal

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
-import Text from './Text';
 import AppBarTab from './AppBarTab'
 import theme from './theme';
 
@@ -19,8 +18,10 @@ const styles = StyleSheet.create({
 
 const AppBar = () => {
     return <View style={styles.container}>
-        <AppBarTab link='/signin' text='Sign in' />
-        <AppBarTab link='/' text='Repositories' />
+        <ScrollView horizontal>
+            <AppBarTab link='/signin' text='Sign in' />
+            <AppBarTab link='/' text='Repositories' />
+        </ScrollView>
     </View>;
 };
 
--- src/components/AppBarTab.jsx
+++ src/components/AppBarTab.jsx
@@ -5,7 +5,7 @@ import Text from './Text';
 
 const styles = StyleSheet.create({
     container: {
-
+        padding: 10
     },
     tab: {
         //
