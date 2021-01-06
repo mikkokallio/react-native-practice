2434fd7dfc8c779bbb693eb43a93c49ae55c6f31
Mikko Kallio <mikko.m.kallio@gmail.com>
2021-01-06 22:35:45 +0200

    fix useSignIn

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -19,15 +19,6 @@ query {
 }
 `;
 
-const ADD_TODO = gql`
-  mutation AddTodo($type: String!) {
-    addTodo(type: $type) {
-      id
-      type
-    }
-  }
-`;
-
 export const GET_ACCESS_TOKEN = gql`
 mutation GetAccessToken($username: String!, $password: String!) {
     authorize(credentials: { username: $username, password: $password }) {
--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,24 +1,14 @@
 import { useMutation } from '@apollo/react-hooks';
 import { GET_ACCESS_TOKEN } from "../graphql/queries";
 
-//where result is the mutations result as it is returned by the useMutation hook 
-//and signIn a function that runs the mutation with a { username, password } object argument. 
-
 const useSignIn = () => {
-    const [mutate, result] = useMutation(GET_ACCESS_TOKEN, {
-        //
-    });
+    const [mutate, result] = useMutation(GET_ACCESS_TOKEN);
 
     const signIn = async ({ username, password }) => {
-        mutate({ variables: { username, password } })
-        //console.log('result',result.client.data)
-        return result;
+        return mutate({ variables: { username, password } });
     };
 
     return [signIn, result];
 };
 
 export default useSignIn;
-
-//This exercise is completed once you can log the user's authorize mutations result after the sign in form has been submitted. 
-//The mutation result should contain the user's access token.
