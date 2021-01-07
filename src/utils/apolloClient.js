7b8a38eaddd506d3be555e41a9acc5b2ffd62fed
Mikko Kallio <mikko.m.kallio@gmail.com>
2021-01-07 22:19:03 +0200

    redirect to repos after login

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,9 +1,21 @@
 import ApolloClient from 'apollo-boost';
 import Constants from 'expo-constants';
 
-const createApolloClient = () => {
+const createApolloClient = (authStorage) => {
   return new ApolloClient({
-    uri: Constants.manifest.extra.apollo_uri,
+    request: async (operation) => {
+      try {
+        const accessToken = await authStorage.getAccessToken();
+        operation.setContext({
+          headers: {
+            authorization: accessToken ? `Bearer ${accessToken}` : '',
+          },
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    uri: Constants.manifest.extra.apollo_uri,    // uri and other options...
   });
 };
 
