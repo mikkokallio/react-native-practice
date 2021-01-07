fa87223f84cecfd3de1ea796d42af68cc20d88a9
Mikko Kallio <mikko.m.kallio@gmail.com>
2021-01-07 22:42:08 +0200

    clean up comments

--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -13,7 +13,6 @@ const useSignIn = () => {
     const [mutate, result] = useMutation(GET_ACCESS_TOKEN);
 
     const signIn = async ({ username, password }) => {
-        //return mutate({ variables: { username, password } });
         const { data } = await mutate({ variables: { username, password } });
         console.log(data.authorize.accessToken);
         await authStorage.setAccessToken(data.authorize.accessToken);
@@ -26,19 +25,3 @@ const useSignIn = () => {
 };
 
 export default useSignIn;
-
-//Improve the useSignIn hook so that it stores the user's access token retrieved from the authorize mutation. 
-//The return value of the hook should not change. 
-
-
-
-
-
-//React Context effectively to find out how to combine the useReducer hook with the context to implement state management.
-// You might find a way to use this knowledge in the upcoming exercises.
-
-//The only change you should make to the SignIn component is that you should redirect the user to 
-//the reviewed repositories list view after a successful sign in. You can achieve this by using the useHistory hook and the history's push method.
-
-//After the authorize mutation has been executed and you have stored the user's access token to the storage, you should reset the Apollo Client's store.
-// This will clear the Apollo Client's cache and re-execute all active queries. You can do this by using the Apollo Client's resetStore method. 
--- src/utils/authStorage.js
+++ src/utils/authStorage.js
@@ -27,12 +27,3 @@ class AuthStorage {
 }
 
 export default AuthStorage;
-
-//We can add an item to the storage using the AsyncStorage.setItem method. 
-//The first argument of the method is the item's key and the second argument its value. 
-//The value must be a string, so we need to serialize non-string values as we did with the JSON.stringify method.
-// The AsyncStorage.getItem method can be used to get an item from the storage. 
-//The argument of the method is the item's key, of which value will be resolved. 
-//The AsyncStorage.removeItem method can be used to remove the item with the provided key from the storage.
-// Next, implement the methods AuthStorage.getAccessToken, AuthStorage.setAccessToken and AuthStorage.removeAccessToken. 
-//Use the namespace variable to give your keys a namespace like we did in the previous example.
