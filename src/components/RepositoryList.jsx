f53a30cb0a408baa9f8a563b47a9ecc0f32e56c5
Mikko Kallio <mikko.m.kallio@gmail.com>
2021-01-09 17:51:22 +0200

    refactor RepositoryList

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -11,11 +11,9 @@ const styles = StyleSheet.create({
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-const RepositoryList = () => {
-    const { repositories } = useRepositories();
-    
+export const RepositoryListContainer = ({ repositories }) => {
     const repositoryNodes = repositories
-        ? repositories.edges.map(edge => edge.node)
+        ? repositories.edges.map((edge) => edge.node)
         : [];
 
     return (
@@ -27,6 +25,12 @@ const RepositoryList = () => {
             )}
         />
     );
+};
+
+const RepositoryList = () => {
+    const { repositories } = useRepositories();
+
+    return <RepositoryListContainer repositories={repositories} />;
 };
 
 export default RepositoryList;
